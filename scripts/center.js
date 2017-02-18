e56809f8623653e4a037badb071342ed99c92310
Jacob Prinz <jacobeliasprinz@gmail.com>
2017-02-18 16:49:49 -0500

    Finally a working canvas and shit

--- scripts/center.js
+++ scripts/center.js
@@ -11,26 +11,24 @@ define(["scripts/libs/only"], function(only){
     var pixelsTall = dimentions.height * canvas.height;
     var pixelsToCenterX = dimentions.x * canvas.width;
     var pixelsToCenterY = dimentions.y * canvas.height;
-    var h = pixelsTall, w = pixelsWide, angle = dimentions.angle;
 
-    var offsetX = (w/2)*Math.cos(angle + Math.PI/2) + (w/2)*Math.cos(angle - Math.PI);
-    var offsetY = (h/2)*Math.sin(angle + Math.PI/2) + (h/2)*Math.sin(angle - Math.PI);
+    ctx.save();
+    ctx.translate( pixelsToCenterX, pixelsToCenterY );
+    ctx.rotate( dimentions.angle );
+    ctx.translate( -pixelsWide/2, -pixelsTall/2 );
+    ctx.drawImage( image, 0, 0 , pixelsWide, pixelsTall);
+    ctx.restore();
 
-    console.log(offsetX, " " , offsetY)
-
-    var x = pixelsToCenterX + offsetX;
-    var y = pixelsToCenterY - offsetY;
-
-    ctx.translate(x,y);
-    ctx.rotate(dimentions.angle)
-    //ctx.drawImage(image, 0, 0, pixelsWide, pixelsTall);
     ctx.drawImage(image, 0, 0, 10, 10);
   }
 
   var angle = 0.0;
   function update(state) {
-    canvas.height = canvas.parentNode.offsetHeight - 10;
-    canvas.width = canvas.parentNode.offsetWidth - 10;
+    var height = canvas.parentNode.offsetHeight - 10;
+    var width = canvas.parentNode.offsetWidth - 10;
+    var better = Math.min(height, width);
+    canvas.height = better;
+    canvas.width = better;
 
     drawImage(earth, {
       x: .5, y:.5, width: 1, height: 1, angle: angle
