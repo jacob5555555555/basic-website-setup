479951c05d5b4b124683b4ab03bc2da27fa02b62
Jacob Prinz <jacobeliasprinz@gmail.com>
2017-02-18 02:55:30 -0500

    gameManager

---
+++ scripts/gameManager.js
@@ -0,0 +1,36 @@
+define([], function(){
+function makeGameManager(){
+    //constants:
+    var TICK_TIME = 50;//millis
+
+
+    var playing = false;
+    function runGame(loopFunction){
+      playing = true;
+      var interval = setInterval(function(){
+        if (playing){
+          loopFunction();
+        }
+      }, TICK_TIME)
+    }
+    function ifPlaying(callback){
+      return function(){
+        if (playing){
+          callback();
+        }
+      };
+    }
+    function setPaused(isPaused){
+      playing = !isPaused
+    }
+    return {
+      ifPlaying: ifPlaying,
+      runGame: runGame,
+      setPaused: setPaused
+    }
+  }
+
+  return {
+    makeGameManager: makeGameManager
+  }
+})
