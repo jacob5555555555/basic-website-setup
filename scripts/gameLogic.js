59e85cf50e72887b6606aaf043e1f4d46a9a8797
robby <robbyef98@gmail.com>
2017-02-19 01:34:56 -0500

    rewrote code

--- scripts/gameLogic.js
+++ scripts/gameLogic.js
@@ -64,145 +64,159 @@ define(["scripts/gameManager", "scripts/center"], function(gameManager, center){
       smelters:0,
       solar:0
     };
+    var things = {
+      pollution: {inputs: {}, outputs: {}},
+      energy: {inputs: {}, outputs: {}},
+      trees: {inputs: {}, outputs: {}},
+      coal: {inputs: {}, outputs: {}},
+      ore: {inputs: {}, outputs: {}},
+      wood: {inputs: {}, outputs: {}},
+      coalreserves:  {inputs: {}, outputs: {}},
+      orereserves:  {inputs: {}, outputs: {}},
+      metal: {inputs: {}, outputs: {}},
+      time: {inputs: {}, outputs: {}},
+      lumberJacks: {inputs: {energy: 1, trees: 5}, outputs: {wood:5}},
+      furnaces: {inputs: {wood: 50}, outputs: {energy:50,pollution:1}},
+      coalmines:{inputs: {energy: 10, coal: 10}, outputs: {coalreserves:10,pollution:5}},
+      coalplants:{inputs: {coalreserves:10}, outputs: {energy:50,pollution:10}},
+      sawmills:{inputs: {trees:100,coalreserves:1}, outputs: {wood:100}},
+      oremines:{inputs: {ore:10, energy:50}, outputs: {orereserves:10,pollution:15}},
+      smelters:{inputs: {orereserves:10,coalreserves:5, energy:25}, outputs: {metal:10,pollution:25}},
+      solar: {inputs: {}, outputs: {energy:250}}
+    }
 
     /*
     cost looks like
     {
-      energy: 10, wood: 100
+    energy: 10, wood: 100
+  }
+  results is a similar object of what gets added
+  */
+  var finalres={};
+  function makeButton(cost, results){
+    function canAdd(){
+      for (var resource in cost){
+        if (gameState[resource] < cost[resource]){
+          return false;
+        }
+      }
+      return true;
     }
-    results is a similar object of what gets added
-    */
-    var finalres={};
-    function makeButton(cost, results){
-      function canAdd(){
-        for (var resource in cost){
-          if (gameState[resource] < cost[resource]){
-            return false;
+    function canSubtract(){
+      for (var resource in results){
+        if (gameState[resource] < results[resource]){
+          return false
+        }
+      }
+      return true;
+    }
+    return {
+      add: game.ifPlaying(function(){
+        if (canAdd()){
+          for (var resource in cost){
+            gameState[resource] -= cost[resource];
+          }
+          for (var resource in results){
+            gameState[resource] += results[resource]
           }
         }
-        return true;
-      }
-      function canSubtract(){
-        for (var resource in results){
-          if (gameState[resource] < results[resource]){
-            return false
+      }),
+      subtract: function(){
+        if(canSubtract()){
+          for (var resource in results){
+            gameState[resource] -= results[resource];
           }
         }
-        return true;
-      }
-      return {
-        add: game.ifPlaying(function(){
-          if (canAdd()){
-            for (var resource in cost){
-              gameState[resource] -= cost[resource];
-            }
-            for (var resource in results){
-              gameState[resource] += results[resource]
-            }
-          }
-        }),
-        subtract: function(){
-            if(canSubtract()){
-              for (var resource in results){
-                gameState[resource] -= results[resource];
-              }
-            }
-        },
-        cost: cost,
-        canAdd: canAdd,
-        canSubtract: canSubtract
-      };
-    }
-
-    var buttons = {
-
-
-      "Lumberjack": {description: makeButton({energy: 500}, {lumberJacks: 1}),detail: "a lumberjack turns trees into logs"},
-      "Furnace": {description: makeButton({energy: 5000}, {furnaces: 1}), detail: "a furnace turns logs into energy"},
-      "Coal mine":  {description: makeButton({energy: 100000, wood: 50000}, {coalmines: 1}), detail: "a coal mine extracts coal from the earth"},
-      "Coal plant":  {description: makeButton({energy: 250000, wood: 100000}, {coalplants: 1}), detail: "a coal plant turns liberated coal into energy"},
-      "Ore mine":  {description: makeButton({energy: 500000, wood: 200000}, {oremines: 1}), detail: "a ore mine extracts ore from the ground"},
-      "Smelter":  {description: makeButton({energy: 1000000, wood:250000}, {smelters: 1}), detail: "a smelter turns ore and coal into metal"},
-      "Solar":  {description: makeButton({metal: 2000, energy:10000000}, {solar: 1}), detail: "a solar panel harnesses the energy of the sun"},
-      "Sawmill":  {description: makeButton({metal: 20}, {sawmills: 1}), detail: "a sawmill to cut wood"}
-
-    }
-
-    //actually running it
-    left.setup(buttons);
-    title.setup(game);
-
-    function logicmath(datum){
-      var ed=0;
-      var td=0;
-      var wd=0;
-      var pd=0;
-      var cmd=0;
-      var crd=0;
-      var md=0;
-      var omd=0;
-      var ord=0;
-      var ljw = Math.min(datum.trees,datum.lumberJacks*ljtu)/ljtu;
-      var siw = Math.min(datum.trees,Math.min(datum.coalreserves,datum.sawmills*sicu)*siwg/sicu)/siwg;
-      var fuw = Math.min(datum.wood,datum.furnaces*fuwu);
-      var cmw = Math.min(datum.coal,datum.coalmines*cmcg)/cmcg;
-      var cpw = Math.min(datum.coalreserves,datum.coalplants*cpcu);
-      var omw = Math.min(datum.ore,datum.oremines*omog)/omog;
-      var smw = Math.min(datum.orereserves,Math.min(datum.coalreserves,datum.smelters*smcu)*smou/smcu)/smou;
-      var tf= tico*Math.pow(datum.time,1.25);
-      var pb= pomc / (pomc+datum.pollution);
-      pd= datum.coalmines*cmpg + datum.coalplants*cppg+ datum.furnaces*fupg +datum.oremines*ompg+ datum.smelters*smpg- podc*datum.pollution;
-      td=trgc*datum.trees*( trmc*pb -datum.trees)-ljw-siw*siwg;
-      ed = fuw*fueg+cpw*cpeg+soeg*datum.solar*pb*pb-ljw*ljeu-cmw*cmeu-omw*omeu-smw*smeu-.01*(1-Math.pow(pb,.1))*datum.energy -siw*siec;
-      wd=ljw*ljtu-fuw+siw*siwg;
-      cmd=-cmw*cmcg;
-      crd=cmw*cmcg-cpw-smcu*smw-sicu*siw;
-      omd=-omog*omw;
-      ord=omog*omw- smou*smw;
-      md= smw*smmg;
-      datum.energy+=ed;
-      datum.wood+=wd;
-      datum.trees+=td;
-      datum.time++;
-      datum.pollution+=pd;
-      datum.coalreserves+=crd;
-      datum.coal+=cmd;
-      datum.ore+=omd;
-      datum.orereserves+=ord;
-      datum.metal+=md;
-      for(key in datum){
-        datum[key]=Math.max(0,datum[key]);
-      }
-    }
-    function calc(stat){
-      var state={};
-      for( key in stat){
-        state[key]=stat[key];
-      }
-      for(var i =0;i<50000;i++){
-        logicmath(state)
-      }
-      return state;
-    }
-
-    function loop(){
-      //logic
-      logicmath(gameState);
-      //update right
-      if(gameState.time%20==0){
-      finalres= calc(gameState);
-    }
-      right.update(gameState,pomc,finalres);
-      title.update(gameState);
-      left.update();
-      center.update(gameState);
-    }
-
-
-    game.runGame(loop);
+      },
+      cost: cost,
+      canAdd: canAdd,
+      canSubtract: canSubtract
+    };
   }
 
-    return {
-      runLogic: runLogic
+  var buttons = {
+
+
+    "Lumberjack": {description: makeButton({energy: 500}, {lumberJacks: 1}),detail: "a lumberjack turns trees into logs"},
+    "Furnace": {description: makeButton({energy: 5000}, {furnaces: 1}), detail: "a furnace turns logs into energy"},
+    "Coal mine":  {description: makeButton({energy: 100000, wood: 50000}, {coalmines: 1}), detail: "a coal mine extracts coal from the earth"},
+    "Coal plant":  {description: makeButton({energy: 250000, wood: 100000}, {coalplants: 1}), detail: "a coal plant turns liberated coal into energy"},
+    "Ore mine":  {description: makeButton({energy: 500000, wood: 200000}, {oremines: 1}), detail: "a ore mine extracts ore from the ground"},
+    "Smelter":  {description: makeButton({energy: 1000000, wood:250000}, {smelters: 1}), detail: "a smelter turns ore and coal into metal"},
+    "Solar":  {description: makeButton({metal: 2000, energy:10000000}, {solar: 1}), detail: "a solar panel harnesses the energy of the sun"},
+    "Sawmill":  {description: makeButton({metal: 20}, {sawmills: 1}), detail: "a sawmill to cut wood"}
+
+  }
+
+  //actually running it
+  left.setup(buttons);
+  title.setup(game);
+  function logicmath(datum){
+    var deltas={};
+    var temset={};
+    for(key in datum){
+      deltas[key]=0;
+      temset[key]=datum[key];
     }
+    for(var key in things){
+      var k=10000000000;
+      for(var inp in things[key].inputs){
+        if(things[key].inputs[inp]!=0){
+          k=Math.min(k,Math.min(Math.floor( temset[inp]/things[key].inputs[inp]),datum[key]));
+        }else{
+          k=0;
+        }
+      }
+      k=Math.min(k,datum[key]);
+
+      for(var inp in things[key].inputs){
+        temset[inp]-=k*things[key].inputs[inp];
+      }
+      for(var out in things[key].outputs){
+        deltas[out]+=k*things[key].outputs[out];
+
+      }
+    }
+    var tval =0.00000001 * datum.trees * ( 100000000*100000/(100000+datum.pollution) - datum.trees);
+    var pval =.0000001*datum.pollution;
+    for(var d in deltas){
+      datum[d]=deltas[d]+temset[d];
+    }
+    datum.trees+=tval;
+    datum.pollution-=pval;
+    datum.time+=1;
+
+  }
+
+  function calc(stat){
+    var state={};
+    for( key in stat){
+      state[key]=stat[key];
+    }
+    for(var i =0;i<50000;i++){
+      logicmath(state)
+    }
+    return state;
+  }
+
+  function loop(){
+    //logic
+    logicmath(gameState);
+    //update right
+    if(gameState.time%20==0){
+      finalres= calc(gameState);
+    }
+    right.update(gameState,pomc,finalres);
+    title.update(gameState);
+    left.update();
+    center.update(gameState);
+  }
+
+
+  game.runGame(loop);
+}
+
+return {
+  runLogic: runLogic
+}
 })
