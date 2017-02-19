b1136330663427267499ff00bb3dcb4a7bea32ca
Jacob Prinz <jacobeliasprinz@gmail.com>
2017-02-19 00:39:26 -0500

    Abooot

--- scripts/title.js
+++ scripts/title.js
@@ -7,8 +7,9 @@ define(["scripts/libs/only", "scripts/htmlUtils", "scripts/popups"], function (o
     };
 
     var timeTag = only.html({p: "Year: " + 1750, css: header});
-    var pause = only.html({button: "Pause", class: "fancyButton"})
-    var html = htmlUtils.spacedColumns([pause, timeTag, {div: ""}], [0.04, 0.90, 0.06]);
+    var pause = only.html({button: "Pause", class: "fancyButton"});
+    var about = only.html({button: "About", class: "fancyButton"})
+    var html = htmlUtils.spacedColumns([pause, timeTag, about], [0.05, 0.90, 0.05]);
 
     function title(state) {
 
@@ -30,6 +31,20 @@ define(["scripts/libs/only", "scripts/htmlUtils", "scripts/popups"], function (o
           game.setPaused(true);
           popups.setPopup(resume);
         }
+      })
+      var str = "Welcome to Earth! You are an omnisc ruler who must power their kingdom";
+      about.addEventListener("click", function(){
+        game.setPaused(true);
+        var text = only.html({div: [
+          {div: [
+            {p: "Welcome to the"}
+          ], css: {
+            background: "white",
+            border: "solid black 1px"
+          }},
+          resume
+        ]})
+        popups.setPopup(text);
       })
     }
 
